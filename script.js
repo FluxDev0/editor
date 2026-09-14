f2b1da93aaa0de29c8916287109082276914d322
FluxDev0 <254105571+FluxDev0@users.noreply.github.com>
2026-09-14 21:30:49 +0200

    easter eggs

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function nextText() {
     });
 }
 
+function lastText() {
+    textNumber--;
+    document.querySelectorAll("span.text").forEach((element) => {
+        element.style = "display: none;";
+        if (element.dataset.n == textNumber) {
+            element.style = "display: block;";
+        }
+    });
+}
+
 document.querySelectorAll("textarea.code").forEach((element) => {
     document.querySelector(`iframe[data-p="${element.dataset.p}"]`).srcdoc = element.value;
     element.addEventListener('keyup', () => {
