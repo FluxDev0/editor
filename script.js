cadd1a48e9bf5f398c497481fa5f6825b70952ef
FluxDev0 <254105571+FluxDev0@users.noreply.github.com>
2026-09-17 15:04:59 +0200

    fix

--- script.js
+++ script.js
@@ -28,12 +28,3 @@ document.querySelectorAll("textarea.code").forEach((element) => {
         document.querySelector(`iframe[data-p="${element.dataset.p}"]`).srcdoc = element.value;
     });
 });
-
-function openWikiPage(pagename) {
-    document.querySelectorAll("body > .wikipage").forEach((element) => {
-        element.style = "display: none;";
-        if (element.dataset.name == pagename) {
-            element.style = "display: block;";
-        }
-    });
-}
